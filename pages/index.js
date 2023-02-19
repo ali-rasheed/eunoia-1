503700c284ad3713c9eed8e687b5570247b8c7b8
Ali Gwaihir <aliasgerrasheed@outlook.com>
2023-02-19 02:01:14 -0800

    |

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,12 @@
+import Spline from '@splinetool/react-spline';
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Spline scene="https://prod.spline.design/CPhaKm7nWx9oK-32/scene.splinecode" />
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
   )
+  
 }
